71acac6329ec1034adedc3dddd8d77f9189ed1f1
JasonLuis <jason20luis@gmail.com>
2023-03-07 18:28:32 -0300

    chore: create component BaseButton

---
+++ src/core/components/BaseButton/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-btn>read more</q-btn>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@500;700&family=Heebo:wght@400;700&family=Inter:wght@400;500;600;700;800;900&display=swap');
+.q-btn {
+  width: 185px;
+  height: 48px;
+  background: #f15d51;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: 4.375px;
+  color: #fffdfa;
+  &:hover {
+    background: #00001a;
+    color: #fffdfa;
+  }
+}
+</style>
